81894e602e47c21c930ae5a4556b8acf8f7d64e2
bryphe <bryphe@outlook.com>
2019-01-28 18:09:48 -0800

    Formatting

--- src/Native/config/discover.ml
+++ src/Native/config/discover.ml
@@ -1,28 +1,21 @@
 type os =
-    | Windows
-            | Mac
-                | Linux
-                    | Unknown
-
+  | Windows 
+  | Mac 
+  | Linux 
+  | Unknown 
 let uname () =
-    let ic = Unix.open_process_in "uname" in
-    let uname = input_line ic in
-    let () = close_in ic in
-    uname;;
-
-let get_os = 
-    match Sys.os_type with
-    | "Win32" -> Windows
-                | _ -> match uname () with
-                        | "Darwin" -> Mac
-                                | "Linux" -> Linux
-                                        | _ -> Unknown
-let c_flags = match get_os with
-    | Mac -> ["-I"; "."; "-x"; "objective-c"]
-    | _ -> []
-;;
-
+  let ic = Unix.open_process_in "uname" in
+  let uname = input_line ic in let () = close_in ic in uname
+let get_os =
+  match Sys.os_type with
+  | "Win32" -> Windows
+  | _ ->
+      (match uname () with
+       | "Darwin" -> Mac
+       | "Linux" -> Linux
+       | _ -> Unknown)
+let c_flags =
+  match get_os with | Mac -> ["-I"; "."; "-x"; "objective-c"] | _ -> []
 let flags = []
-;;
-Configurator.V1.Flags.write_sexp "c_flags.sexp" c_flags;
-Configurator.V1.Flags.write_sexp "flags.sexp" flags;
+;;Configurator.V1.Flags.write_sexp "c_flags.sexp" c_flags;
+  Configurator.V1.Flags.write_sexp "flags.sexp" flags
