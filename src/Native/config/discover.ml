64774424f03761faff1536a59186d27d5be4414a
Akin909 <akin.sowemimo@gmail.com>
2019-01-31 08:55:45 +0000

    Add revery gtk header and alert implementation

--- src/Native/config/discover.ml
+++ src/Native/config/discover.ml
@@ -1,3 +1,27 @@
+module C = Configurator.V1
+
+let f elem = Printf.printf "I'm looking at element %s now\n" elem
+
+let () = 
+  C.main ~name:"linux" (fun c ->
+    let default : C.Pkg_config.package_conf =
+      { libs   = []
+      ; cflags = []
+      }
+    in
+    let conf  = match C.Pkg_config.get c with
+    | None -> default
+    | Some pc ->
+        match (C.Pkg_config.query pc ~package:"gtk+-3.0") with
+        | None -> default
+        | Some conf -> conf
+    in
+
+    C.Flags.write_sexp "flags.sexp" [];
+    C.Flags.write_sexp "c_flags.sexp" conf.cflags;
+    C.Flags.write_sexp "c_library_flags.sexp" conf.libs)
+
+  (**
 type os =
   | Windows 
   | Mac 
@@ -15,7 +39,15 @@ let get_os =
        | "Linux" -> Linux
        | _ -> Unknown)
 let c_flags =
-  match get_os with | Mac -> ["-I"; "."; "-x"; "objective-c"] | _ -> []
+  match get_os with | Mac -> ["-I"; "."; "-x"; "objective-c"] | Linux -> 
+    ["`pkg-config --cflags gtk+-3.0`"; "`pkg-config --libs gtk+-3.0`"] | _ -> []
 let flags = []
-;;Configurator.V1.Flags.write_sexp "c_flags.sexp" c_flags;
-  Configurator.V1.Flags.write_sexp "flags.sexp" flags
+;;
+
+let write_sexp fn eles =
+   C.Flags.write_sexp fn eles
+in
+
+C.Flags.write_sexp "c_flags.sexp" c_flags;
+C.Flags.write_sexp "flags.sexp" flags
+*)
