c82ed476c3936e43786c9091ca35769ac1855549
Akin909 <akin.sowemimo@gmail.com>
2019-02-01 16:58:23 +0000

    pass message to dialog function

--- src/Native/dialog_gtk.c
+++ src/Native/dialog_gtk.c
@@ -4,9 +4,8 @@
 void revery_alert_gtk(void *pWin, const char *szMessage) {
   GtkDialogFlags flags = GTK_DIALOG_DESTROY_WITH_PARENT;
   GtkWidget *dialog;
-  dialog = gtk_message_dialog_new(NULL, flags, GTK_MESSAGE_ERROR,
-                                  GTK_BUTTONS_CLOSE, "Error reading “%s”: %s",
-                                  "filename", g_strerror(errno));
+  dialog = gtk_message_dialog_new(NULL, flags, GTK_MESSAGE_INFO,
+                                  GTK_BUTTONS_CLOSE, szMessage);
   gtk_dialog_run(GTK_DIALOG(dialog));
   gtk_widget_destroy(dialog);
 }
