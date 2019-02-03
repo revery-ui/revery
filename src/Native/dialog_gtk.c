ec8c790dc470c4964f9940b8408923b62221a69a
Akin909 <akin.sowemimo@gmail.com>
2019-02-03 22:33:40 +0000

    create application window for dialog widget

--- src/Native/dialog_gtk.c
+++ src/Native/dialog_gtk.c
@@ -1,11 +1,27 @@
-#include <errno.h>
+#include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
-void revery_alert_gtk(void *pWin, const char *szMessage) {
-  GtkDialogFlags flags = GTK_DIALOG_DESTROY_WITH_PARENT;
+static void activate(GtkApplication *app, gpointer user_data) {
+  GtkWidget *window;
   GtkWidget *dialog;
-  dialog = gtk_message_dialog_new(NULL, flags, GTK_MESSAGE_INFO,
-                                  GTK_BUTTONS_CLOSE, szMessage);
+
+  window = gtk_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW(window), "revery gtk");
+
+  GtkDialogFlags flags = GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT;
+  dialog = gtk_dialog_new_with_buttons("Revery Dialog", GTK_WINDOW(window),
+                                       flags, _("_OK"), GTK_RESPONSE_ACCEPT,
+                                       _("_Cancel"), GTK_RESPONSE_REJECT, NULL);
+
   gtk_dialog_run(GTK_DIALOG(dialog));
+  gtk_widget_show_all(dialog);
   gtk_widget_destroy(dialog);
+}
+
+void revery_alert_gtk(void *pWin, const char *szMessage) {
+  GtkApplication *app;
+  app = gtk_application_new("org.gtk.revery", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  g_application_run(G_APPLICATION(app), 0, 0);
+  g_object_unref(app);
 }
