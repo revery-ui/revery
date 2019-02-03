099f5de46f0ce372de89e611ad986693f5e81fe3
Akin909 <akin.sowemimo@gmail.com>
2019-02-03 22:50:18 +0000

    rename function to activate as that is what it must be called

--- src/Native/dialog_gtk.c
+++ src/Native/dialog_gtk.c
@@ -1,7 +1,8 @@
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
-static void show_dialog(GtkApplication *app, gpointer user_data) {
+// The callback to g_signal_connect MUST be an `activate` function
+static void activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window;
   GtkWidget *dialog;
 
@@ -14,14 +15,16 @@ static void show_dialog(GtkApplication *app, gpointer user_data) {
                                        _("_Cancel"), GTK_RESPONSE_REJECT, NULL);
 
   gtk_dialog_run(GTK_DIALOG(dialog));
-  gtk_widget_show_all(dialog);
   gtk_widget_destroy(dialog);
 }
 
 void revery_alert_gtk(void *pWin, const char *szMessage) {
+  // TODO: figure out how to convert the pointer from an X11 window handle
+  // to a GTK window, see (for inspiration):
+  // https://gist.github.com/mmozeiko/2401933b1fa89e5d5bd238b33eab0465
   GtkApplication *app;
   app = gtk_application_new("org.gtk.revery", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "show_dialog", G_CALLBACK(show_dialog), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   g_application_run(G_APPLICATION(app), 0, 0);
   g_object_unref(app);
 }
