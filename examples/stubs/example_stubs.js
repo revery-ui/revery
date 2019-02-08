5148646f649f1b9009ee53373f2d7d978b491213
Bryan Phelps <bryphe@outlook.com>
2019-02-08 11:39:23 -0800

    Initial "playground" for examples (#314)
    Initial "playground" for examples

---
+++ examples/stubs/example_stubs.js
@@ -0,0 +1,7 @@
+// Provides: revery_example_notify_changed
+function revery_example_notify_changed(src) {
+    var window = joo_global_object.window;
+    if (window && window["__revery_playground_example_notify_changed"]) {
+        window["__revery_playground_example_notify_changed"](caml_to_js_string(src));
+    }
+}
