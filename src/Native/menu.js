4bb3090e030189fbfa1a2e79d9e4032d7d5b86c4
Ã‰lie BRAMI <cadeaudeelie@gmail.com>
2019-06-24 02:28:59 +0200

    implement all missing js primitives no-ops

--- src/Native/menu.js
+++ src/Native/menu.js
@@ -10,11 +10,67 @@ function revery_create_menu() {
     return;
 }
 
-// Provides: revery_add_string_item_menu
-function revery_add_string_item_menu(vMenu, pMessage) {
+// Provides: revery_create_sub_menu
+function revery_create_sub_menu() {
     // no-op
-    window.console.error("WARNING - Not implemented: revery_add_string_item_menu_js");
+    window.console.error("WARNING - Not implemented: revery_create_sub_menu_js");
     return;
+}
+
+// Provides: revery_create_popup_menu
+function revery_create_popup_menu() {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_create_popup_menu_js");
+    return;
+}
+
+// Provides: revery_add_string_item_sub_menu
+function revery_add_string_item_sub_menu(vMenu, vUid, pMessage) {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_add_string_item_sub_menu_js");
+    return false;
+}
+
+// Provides: revery_add_string_item_popup_menu
+function revery_add_string_item_popup_menu(vMenu, vUid, vMessage) {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_add_string_item_popup_menu_js");
+    return false;
+}
+
+// Provides: revery_add_separator_sub_menu
+function revery_add_separator_sub_menu(vMenu) {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_add_separator_sub_menu_js");
+    return false;
+}
+
+// Provides: revery_add_separator_popup_menu
+function revery_add_separator_popup_menu(vMenu) {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_add_separator_popup_menu_js");
+    return false;
+}
+
+// Provides: revery_add_sub_menu
+function revery_add_sub_menu(vMenu, vSub, vMessage) {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_add_sub_menu_js");
+    return false;
+}
+
+// Provides: revery_add_sub_menu_sub_menu
+function revery_add_sub_menu_sub_menu(vMenu, vSub, vMessage) {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_add_sub_menu_sub_menu_js");
+    return false;
+}
+
+// Provides: revery_add_sub_menu_popup_menu
+function revery_add_sub_menu_popup_menu(vMenu, vSub, vMessage) {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_add_sub_menu_popup_menu_js");
+    return false;
 }
 
 // Provides: revery_assign_menu
@@ -22,4 +78,25 @@ function revery_assign_menu(vWindow, vMenu) {
     // no-op
     window.console.error("WARNING - Not implemented: revery_assign_menu_js");
     return;
+}
+
+// Provides: revery_popup_sub_menu
+function revery_popup_sub_menu(vWindow, vMenu, vX, vY) {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_popup_sub_menu_js");
+    return false;
+}
+
+// Provides: revery_get_application_menu
+function revery_get_application_menu(vWindow, vList) {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_get_application_menu_js");
+    return;
+}
+
+// Provides: revery_refresh_menu_bar
+function revery_refresh_menu_bar(vWindow) {
+    // no-op
+    window.console.error("WARNING - Not implemented: revery_refresh_menu_bar_js");
+    return false;
 }
