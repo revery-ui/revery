1a85f7885f0b543e01d904e9f37cdc5f3c44a777
Et7f3 <cadeaudeelie@gmail.com>
2019-09-29 03:22:49 +0200

    fix warnings in cocoa and remove unused variables

--- src/Native/dialog_cocoa.c
+++ src/Native/dialog_cocoa.c
@@ -4,9 +4,8 @@
 #import <Cocoa/Cocoa.h>
 
 void revery_alert_cocoa(void *pWin, const char *szMessage) {
-    NSWindow* pCocoaWin  = (NSWindow *)pWin;
+    (void)pWin;
 
-    NSView *view = [[NSView alloc] init];
     NSAlert *alert = [[NSAlert alloc] init];
     NSString *message = [NSString stringWithUTF8String:szMessage];
     [alert addButtonWithTitle:@"Ok"];
