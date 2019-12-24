ebb41cba6facdfa3a0d01d96b22395f89b48cef5
Et7f3 <cadeaudeelie@gmail.com>
2019-12-24 03:53:03 +0100

    Add revery_create_menu.

--- src/Native/ReveryWin32.h
+++ src/Native/ReveryWin32.h
@@ -1,1 +1,2 @@
-   void revery_alert_win32(void *pWin, const char* szMessage);
+void revery_alert_win32(void *pWin, const char *szMessage);
+CAMLprim value revery_create_menu_win32(void);
--- src/Native/menu.c
+++ src/Native/menu.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
+
 #include <caml/mlvalues.h>
+#include <caml/memory.h>
 
 #ifdef WIN32
 #include "ReveryWin32.h"
@@ -19,4 +22,22 @@ CAMLprim value revery_menuSupported()
 #else
   return Val_false;
 #endif
+}
+
+CAMLprim value revery_create_menu()
+{
+    CAMLparam0();
+    value ret;
+
+#ifdef WIN32
+    ret = revery_create_menu_win32();
+#elif __APPLE__
+    fprintf(stderr, "WARNING - Not implemented: revery_create_menu_cocoa");
+#elif __linux__
+    fprintf(stderr, "WARNING - Not implemented: revery_create_menu_gtk");
+#else
+    fprintf(stderr, "WARNING - Not implemented: revery_create_menu");
+#endif
+
+    CAMLreturn(ret);
 }
