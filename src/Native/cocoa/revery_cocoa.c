97daf16a8e023fe430f3bd49ede7714237b6782a
Zach Baylin <me@zachbayl.in>
2019-12-24 19:25:25 -0500

    removed c-based optcomp

--- src/Native/cocoa/revery_cocoa.c
+++ src/Native/cocoa/revery_cocoa.c
@@ -3,14 +3,10 @@
 #include <caml/memory.h>
 #include <caml/mlvalues.h>
 
-#ifdef __APPLE__
 #import "ReveryAppDelegate.h"
-#endif
 
 CAMLprim value revery_cocoaSetAppDelegate() {
-#ifdef __APPLE__
   ReveryAppDelegate *delegate = [ReveryAppDelegate new];
   [NSApp setDelegate:delegate];
-#endif
   return Val_unit;
 }
