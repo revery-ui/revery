97248569a8d48a0fee0ee33b03c134423aff8971
Zach Baylin <me@zachbayl.in>
2019-12-29 15:52:44 -0500

    depended on esy-astyle, modified format script

--- .ci/format.sh
+++ .ci/format.sh
@@ -1,1 +1,16 @@
-find . -name "*.re" -type f -exec refmt --in-place {} \;
+set -e
+OS=$(uname)
+
+if [[ $OS =~ "CYWGIN" ]];
+then
+    find . -name "*.re" -type f -exec refmt --in-place {} \;
+else
+    dune build @fmt --auto-promote
+fi
+
+native_output=$(find -E . -regex '.*\.(c|h)' -type f -not -path "*_esy/*" -exec astyle -n -Q --style=google --s2 {} \;)
+
+if [[ $native_output ]];
+then
+    printf "Formatted the following native stubs:\n$native_output"; exit 1
+fi
