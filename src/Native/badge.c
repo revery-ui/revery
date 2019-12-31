eab7c0a764e349d255eb1b3729034e70f0c42770
Zach Baylin <me@zachbayl.in>
2019-12-30 22:59:00 -0500

    formatted the files

--- src/Native/badge.c
+++ src/Native/badge.c
@@ -17,23 +17,23 @@
 #endif
 
 CAMLprim value revery_setBadgeNumber(value vBadgeNum) {
-  CAMLparam1(vBadgeNum);
+    CAMLparam1(vBadgeNum);
 
-  int badgeNum = Int_val(vBadgeNum);
-  char badgeNumStr[(int)((ceil(log10(badgeNum)) + 1) * sizeof(char))];
-  sprintf(badgeNumStr, "%d", badgeNum);
+    int badgeNum = Int_val(vBadgeNum);
+    char badgeNumStr[(int)((ceil(log10(badgeNum)) + 1) * sizeof(char))];
+    sprintf(badgeNumStr, "%d", badgeNum);
 
 #ifdef __APPLE__
-  revery_setBadgeNumber_cocoa(badgeNumStr);
+    revery_setBadgeNumber_cocoa(badgeNumStr);
 #else
-  (void)badgeNumStr;
+    (void)badgeNumStr;
 #endif
 
-  CAMLreturn(Val_unit);
+    CAMLreturn(Val_unit);
 }
 
 void revery_hideBadge() {
-  #ifdef __APPLE__
+#ifdef __APPLE__
     revery_hideBadge_cocoa();
-  #endif
+#endif
 }
--- src/Native/badge_cocoa.c
+++ src/Native/badge_cocoa.c
@@ -4,13 +4,13 @@
 #import <Cocoa/Cocoa.h>
 
 void revery_hideBadge_cocoa() {
-  [[NSApp dockTile] setBadgeLabel:NULL];
+    [[NSApp dockTile] setBadgeLabel:NULL];
 }
 
 void revery_setBadgeNumber_cocoa(char *badgeNumStr) {
-  NSString *nsBadgeNumStr = [NSString stringWithUTF8String:badgeNumStr];
-  [[NSApp dockTile] setBadgeLabel:nsBadgeNumStr];
-  [nsBadgeNumStr release];
+    NSString *nsBadgeNumStr = [NSString stringWithUTF8String:badgeNumStr];
+    [[NSApp dockTile] setBadgeLabel:nsBadgeNumStr];
+    [nsBadgeNumStr release];
 }
 
 #endif
