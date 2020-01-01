c95a38621b0c6eb9585c39253130e8bdd6a8ccb7
Zach Baylin <me@zachbayl.in>
2020-01-01 16:53:27 -0500

    Fix formatting bugs on Linux and Windows (#702)
    * fixed formatting on Linux
    
    * ran script through shellcheck
    
    * fixed windows with cygwin-style paths

--- .ci/format.sh
+++ .ci/format.sh
@@ -1,18 +1,28 @@
-set -e
+#!/usr/bin/env bash
 OS=$1
 
 dune build @fmt --auto-promote
 
-if [[ $OS == "windows" ]];
+caml_output=$?
+
+if [[ $OS == "windows" ]]
 then
-    # Prevents "Environment too big" error on Windows
-    files=$(/usr/bin/find ./ -type f \( -iname \*.c -o -iname \*.h \) -not -path "*_esy/*")
-    native_output=$(astyle -n -Q --style=java --s4 $files)
-else
+    files=$(/usr/bin/find $(pwd) -type f \( -iname \*.c -o -iname \*.h \) -not -path "*_esy/*")
+    native_output=$(astyle -n -Q --style=java --s4 $(cygpath -w $files))
+elif [[ $OS == "darwin" ]]
+then
     native_output=$(find -E . -regex '.*\.(c|h)' -type f -not -path "*_esy/*" -exec astyle -n -Q --style=java --s4 {} \;)
+elif [[ $OS == "linux" ]]
+then
+    native_output=$(find ./ -type f \( -iname \*.c -o -iname \*.h \) -not -path "*_esy/*" -exec astyle -n -Q --style=java --s4 {} \;)
 fi
 
-if [[ $native_output ]];
+if [[ $native_output ]]
 then
-    printf "Formatted the following native stubs:\n$native_output"; exit 1
+    printf "\nFormatted the following native stubs:\n%s" "$native_output"
+fi
+
+if [[ $caml_output != 0 ]] || [[ $native_output != "" ]]
+then
+    exit 1
 fi
