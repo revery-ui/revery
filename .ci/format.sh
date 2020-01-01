0bbb52e58c7453754b926295e9731d80baf56919
Zach Baylin <me@zachbayl.in>
2020-01-01 11:36:01 -0800

    fixed formatting on Linux

--- .ci/format.sh
+++ .ci/format.sh
@@ -1,18 +1,23 @@
+#!/bin/bash
 set -e
 OS=$1
 
 dune build @fmt --auto-promote
 
-if [[ $OS == "windows" ]];
+if [ $OS == "windows" ]
 then
     # Prevents "Environment too big" error on Windows
     files=$(/usr/bin/find ./ -type f \( -iname \*.c -o -iname \*.h \) -not -path "*_esy/*")
     native_output=$(astyle -n -Q --style=java --s4 $files)
-else
+elif [ $OS == "darwin" ]
+then
     native_output=$(find -E . -regex '.*\.(c|h)' -type f -not -path "*_esy/*" -exec astyle -n -Q --style=java --s4 {} \;)
+elif [ $OS == "linux" ]
+then
+    native_output=$(find ./ -type f \( -iname \*.c -o -iname \*.h \) -not -path "*_esy/*" -exec astyle -n -Q --style=java --s4 {} \;)
 fi
 
-if [[ $native_output ]];
+if [ $native_output ];
 then
     printf "Formatted the following native stubs:\n$native_output"; exit 1
 fi
