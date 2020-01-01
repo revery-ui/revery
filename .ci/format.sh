a17311cfacaa2167bc363ff1aff5e8700c4ddd25
Zach Baylin <me@zachbayl.in>
2020-01-01 14:35:09 -0600

    fixed windows with cygwin-style paths

--- .ci/format.sh
+++ .ci/format.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 OS=$1
 
 dune build @fmt --auto-promote
@@ -7,9 +7,8 @@ caml_output=$?
 
 if [[ $OS == "windows" ]]
 then
-    # Prevents "Environment too big" error on Windows
-    files=$(/usr/bin/find ./ -type f \( -iname \*.c -o -iname \*.h \) -not -path "*_esy/*")
-    native_output=$(astyle -n -Q --style=java --s4 "$files")
+    files=$(/usr/bin/find $(pwd) -type f \( -iname \*.c -o -iname \*.h \) -not -path "*_esy/*")
+    native_output=$(astyle -n -Q --style=java --s4 $(cygpath -w $files))
 elif [[ $OS == "darwin" ]]
 then
     native_output=$(find -E . -regex '.*\.(c|h)' -type f -not -path "*_esy/*" -exec astyle -n -Q --style=java --s4 {} \;)
