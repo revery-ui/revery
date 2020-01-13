18cf5591e0c5825e8256fbff863836b66e9aab21
Zach Baylin <me@zachbayl.in>
2020-01-12 21:24:20 -0600

    add comments and ellipsis

--- src/Native/icon_win32.c
+++ src/Native/icon_win32.c
@@ -37,47 +37,60 @@ void revery_hideIconProgress_win32(void *win, void *ih) {
 void revery_setIconBadge_win32(void *win, void *ih, char *badgeStr) {
     HWND window = (HWND)win;
     ITaskbarList3 *iconHandle = (ITaskbarList3 *)ih;
+    // Load the badge template into memory
     HBITMAP bmp = (HBITMAP)LoadImage(NULL, "badge.bmp", IMAGE_BITMAP, 0, 0,
                                      LR_LOADFROMFILE);
 
     char *displayStr;
 
+    // Display the full string if it's 3 chars or less
     if (strlen(badgeStr) <= 3) {
         displayStr = badgeStr;
     } else {
+        // Malloc a string and add the first two characters...
         displayStr = (char *) malloc((6) * sizeof(char));
         displayStr[0] = badgeStr[0];
         displayStr[1] = badgeStr[1];
+        // ...an ellipsis...
         displayStr[2] = displayStr[3] = displayStr[4] = '.';
+        // ...and a null termination.
         displayStr[5] = '\0';
     }
 
-    printf("Display string: %s\n", displayStr);
-
+    // Create a rect to paint the text into on top of the bitmap
     RECT rect;
     rect.left = 0;
+    // Center the text in the rect
     rect.top = 6;
     rect.bottom = 32;
     rect.right = 32;
     HDC hdc = CreateCompatibleDC(NULL);
+    // Load the bitmap, and save the old one so we don't leak it
     HBITMAP oldBmp = (HBITMAP)SelectObject(hdc, bmp);
-    HFONT NewFont = CreateFont(
+    HFONT hFont = CreateFont(
         20, 0, 0, 0, FW_BOLD, 0, 0, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
         0, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, "Arial");
-    HBRUSH NewBrush = CreateSolidBrush(RGB(255, 0, 0));
-    SelectObject(hdc, NewFont);
-    SelectObject(hdc, NewBrush);
+    SelectObject(hdc, hFont);
+    // Make sure the text background is transparent
     SetBkMode(hdc, TRANSPARENT);
     SetTextColor(hdc, RGB(255, 255, 255));
     DrawText(hdc, displayStr, strlen(displayStr), &rect, DT_CENTER | DT_WORDBREAK);
-    DeleteObject(NewFont);
-    DeleteObject(NewBrush);
+    // Delete the font and the old bitmap
+    DeleteObject(hFont);
     SelectObject(hdc, oldBmp);
+
+    // This is to convert the HBITMAP into an HICON
     HIMAGELIST imageList = ImageList_Create(32, 32, ILC_COLOR32, 1, 1);
     ImageList_Add(imageList, bmp, NULL);
     HICON hIcon = ImageList_GetIcon(imageList, 0, ILD_IMAGE);
+    
+    // Set the icon
     iconHandle->lpVtbl->SetOverlayIcon(iconHandle, window, hIcon,
                                        L"Badge number");
+
+    DeleteObject(imageList);
+
+    // Release the DC
     ReleaseDC(NULL, hdc);
 
     if (strlen(badgeStr) > 3) {
