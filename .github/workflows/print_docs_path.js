7eaa1b5bfcf7af8d12b1a9ba0486a6c276737b68
Et7f3 <cadeaudeelie@gmail.com>
2020-02-22 16:05:58 +0100

    Update print_docs_path.js

--- .github/workflows/print_docs_path.js
+++ .github/workflows/print_docs_path.js
@@ -2,7 +2,7 @@ const path = require("path");
 const cp = require("child_process");
 
 const docsPath = cp.execSync(
-  `esy @doc echo "#{self.target_dir / 'default' / '_doc' / '_html'}"`
+  `esy @doc --release echo "#{self.target_dir / 'default' / '_doc' / '_html'}"`
 );
 const relativeDocsPath = path.relative(process.cwd(), docsPath.toString());
 
