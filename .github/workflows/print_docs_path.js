9bfa33aa7a3f10ad989f1e9196f9bdcad2c884d9
Et7f3 <cadeaudeelie@gmail.com>
2020-02-23 08:13:22 +0100

    fix quote

--- .github/workflows/print_docs_path.js
+++ .github/workflows/print_docs_path.js
@@ -2,7 +2,7 @@ const path = require("path");
 const cp = require("child_process");
 
 const docsPath = cp.execSync(
-  `esy @ci print"`
+  `esy @ci print`
 );
 const relativeDocsPath = path.relative(process.cwd(), docsPath.toString());
 
