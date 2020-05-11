9ff20650620dd71647c11280368f77d8d9f0b8ae
Zach Baylin <zbaylin@outrunlabs.com>
2020-05-11 14:53:10 -0400

    Formatting

--- src/Native/shell.c
+++ src/Native/shell.c
@@ -16,15 +16,15 @@
 #endif
 
 CAMLprim value revery_openItemAtURL(value vURL) {
-  CAMLparam1(vURL);
+    CAMLparam1(vURL);
 
-  const char *url_string = String_val(vURL);
-  int success = 0;
+    const char *url_string = String_val(vURL);
+    int success = 0;
 #ifdef __APPLE__
-  success = revery_openItemAtURL_cocoa(url_string);
+    success = revery_openItemAtURL_cocoa(url_string);
 #else
-  fprintf(stderr, "WARNING: %s is not implemented on this platform.", __func__);
-  success = 0;
+    fprintf(stderr, "WARNING: %s is not implemented on this platform.", __func__);
+    success = 0;
 #endif
-  CAMLreturn(Val_bool(success));
+    CAMLreturn(Val_bool(success));
 }
