5af4f063dab58a4265392c104c278d29080c13c8
Github Runner <runner@runner.github.com>
2020-05-12 14:18:22 +0000

    format,

--- src/Native/shell_gtk.c
+++ src/Native/shell_gtk.c
@@ -3,11 +3,11 @@
 
 int revery_openURL_gtk(const char *url_string) {
     return (int)gtk_show_uri_on_window(
-                      NULL, // We don't have a parent window (and we don't need one)
-                      url_string,
-                      gtk_get_current_event_time(),
-                      NULL
-                  );
+               NULL, // We don't have a parent window (and we don't need one)
+               url_string,
+               gtk_get_current_event_time(),
+               NULL
+           );
 }
 
 int revery_openFile_gtk(const char *path_string) {
