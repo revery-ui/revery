e6bec683d21c7da92e5a423f8ded31e8beda00ab
Zach Baylin <zbaylin@outrunlabs.com>
2020-08-08 12:32:12 -0400

    Format: add dune files to formatting script

--- .ci/format.sh
+++ .ci/format.sh
@@ -7,22 +7,42 @@ caml_output=$?
 
 if [[ $OS == "windows" ]]
 then
-    files=$(/usr/bin/find $(pwd) -type f \( -iname \*.c -o -iname \*.h -o -iname \*.cpp \) -not -path "*_esy/*")
-    native_output=$(astyle -n -Q --style=java --s4 $(cygpath -w $files))
+    native_files=$(/usr/bin/find $(pwd) -type f \( -iname \*.c -o -iname \*.h -o -iname \*.cpp \) -not -path "*_esy/*")
+    native_output=$(astyle -n -Q --style=java --s4 $(cygpath -w $native_files))
+
+    dune_files=$(/usr/bin/find $(pwd) -type f \( -iname .*\dune \) -not -path "*_esy/*")
 elif [[ $OS == "darwin" ]]
 then
     native_output=$(find -E . -regex '.*\.(c|h|cpp)' -type f -not -path "*_esy/*" -exec astyle -n -Q --style=java --s4 {} \;)
+    
+    dune_files=$(find -E . -regex '.*\dune' -type f -not -path "*_esy/*" -not -path "./.git/*")
 elif [[ $OS == "linux" ]]
 then
     native_output=$(find ./ -type f \( -iname \*.c -o -iname \*.h -o -iname \*.cpp \) -not -path "*_esy/*" -exec astyle -n -Q --style=java --s4 {} \;)
+
+    dune_files=$(find ./ -type f \( -iname .*\dune \) -not -path "*_esy/*")
 fi
+
+dune_output=0
+for dune_file in $dune_files
+do
+    pre=$(cat $dune_file)
+    post=$(dune format-dune-file $dune_file)
+    diff <(echo "$pre") <(echo "$post")
+    diff_output=$?
+    if [[ $diff_output != 0 ]]
+    then
+        dune_output=1
+        echo "$post" > $dune_file
+    fi
+done
 
 if [[ $native_output ]]
 then
     printf "\nFormatted the following native stubs:\n%s\n" "$native_output"
 fi
 
-if [[ $caml_output != 0 ]] || [[ $native_output != "" ]]
+if [[ $caml_output != 0 ]] || [[ $native_output != "" ]] || [[ $dune_output != 0 ]]
 then
     exit 1
 fi
