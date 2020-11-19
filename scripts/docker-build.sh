7efa8851617d69135522823950b3da2e64473a16
Bryan Phelps <bryphe@outrunlabs.com>
2020-11-19 08:02:58 -0800

    Fix CentOS build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,5 +1,10 @@
 source /opt/rh/llvm-toolset-7.0/enable
 clang -v
 
+# Workaround for: https://github.com/esy/esy/issues/1227
+# Concurrent fetch seems to cause hang on Docker in Azure Pipelines..
+export ESY__BUILD_CONCURRENCY=1
+export ESY__FETCH_CONCURRENCY=1
+
 esy install
 esy build
