7576f9aec406777a6aa40630e892c7038f85c6df
Bryan Phelps <bryphe@outrunlabs.com>
2020-11-20 17:27:05 -0800

    Formatting

--- src/Native/dialog.c
+++ src/Native/dialog.c
@@ -22,136 +22,136 @@
 
 CAMLprim value revery_alertSupported() {
 #if defined(USE_WIN32) || defined(USE_COCOA) || defined(USE_GTK)
-  return Val_true;
+    return Val_true;
 #else
-  return Val_false;
+    return Val_false;
 #endif
 }
 
 CAMLprim value revery_alert(value vWindow, value vMessage) {
-  CAMLparam2(vWindow, vMessage);
-  const char *szMessage = String_val(vMessage);
-  void *pWin = (void *)vWindow;
+    CAMLparam2(vWindow, vMessage);
+    const char *szMessage = String_val(vMessage);
+    void *pWin = (void *)vWindow;
 
 #ifdef USE_WIN32
-  revery_alert_win32(pWin, szMessage);
+    revery_alert_win32(pWin, szMessage);
 #elif USE_COCOA
-  revery_alert_cocoa(pWin, szMessage);
+    revery_alert_cocoa(pWin, szMessage);
 #elif USE_GTK
-  revery_alert_gtk(pWin, szMessage);
+    revery_alert_gtk(pWin, szMessage);
 #else
-  printf("WARNING - Not implemented: alert");
-  UNUSED(szMessage);
-  UNUSED(pWin);
+    printf("WARNING - Not implemented: alert");
+    UNUSED(szMessage);
+    UNUSED(pWin);
 #endif
-  return Val_unit;
+    return Val_unit;
 }
 
 CAMLprim value revery_alertOpenFiles_native(
     value vStartDirectory, value vFileTypes, value vAllowMultiple,
     value vCanChooseFiles, value vCanChooseDirectories, value vShowHidden,
     value vButtonText, value vTitle, value vUnit) {
-  CAMLparam5(vStartDirectory, vFileTypes, vAllowMultiple, vCanChooseFiles,
-             vCanChooseDirectories);
-  CAMLxparam3(vButtonText, vTitle, vUnit);
+    CAMLparam5(vStartDirectory, vFileTypes, vAllowMultiple, vCanChooseFiles,
+               vCanChooseDirectories);
+    CAMLxparam3(vButtonText, vTitle, vUnit);
 
-  const char *startDirectory;
-  const char *buttonText;
+    const char *startDirectory;
+    const char *buttonText;
 
-  // Initialize an array of filetypes
-  const char **fileTypes = NULL;
-  int fileTypesSize = 0;
+    // Initialize an array of filetypes
+    const char **fileTypes = NULL;
+    int fileTypesSize = 0;
 
-  // title from OCaml -> C
-  const char *title;
-  if (vTitle != Val_none)
-    title = String_val(Some_val(vTitle));
-  else
-    title = NULL;
+    // title from OCaml -> C
+    const char *title;
+    if (vTitle != Val_none)
+        title = String_val(Some_val(vTitle));
+    else
+        title = NULL;
 
-  int allowMultiple = Bool_val(vAllowMultiple);
-  int canChooseFiles = Bool_val(vCanChooseFiles);
-  int canChooseDirectories = Bool_val(vCanChooseDirectories);
-  int showHidden = Bool_val(vShowHidden);
+    int allowMultiple = Bool_val(vAllowMultiple);
+    int canChooseFiles = Bool_val(vCanChooseFiles);
+    int canChooseDirectories = Bool_val(vCanChooseDirectories);
+    int showHidden = Bool_val(vShowHidden);
 
-  if (vFileTypes != Val_none) {
-    CAMLlocal1(camlArr);
-    camlArr = Some_val(vFileTypes);
-    fileTypesSize = Wosize_val(camlArr);
+    if (vFileTypes != Val_none) {
+        CAMLlocal1(camlArr);
+        camlArr = Some_val(vFileTypes);
+        fileTypesSize = Wosize_val(camlArr);
 
-    // Allocate space for an array
-    fileTypes = (const char **)malloc(sizeof(*fileTypes) * fileTypesSize);
+        // Allocate space for an array
+        fileTypes = (const char **)malloc(sizeof(*fileTypes) * fileTypesSize);
 
-    // Populate the array with the CAML array;
-    for (int i = 0; i < fileTypesSize; i++) {
-      const char *str = String_val(Field(camlArr, i));
-      fileTypes[i] = str;
+        // Populate the array with the CAML array;
+        for (int i = 0; i < fileTypesSize; i++) {
+            const char *str = String_val(Field(camlArr, i));
+            fileTypes[i] = str;
+        }
     }
-  }
 
-  if (vStartDirectory != Val_none) {
-    startDirectory = String_val(Some_val(vStartDirectory));
-  } else {
-    startDirectory = NULL;
-  }
+    if (vStartDirectory != Val_none) {
+        startDirectory = String_val(Some_val(vStartDirectory));
+    } else {
+        startDirectory = NULL;
+    }
 
-  if (vButtonText != Val_none) {
-    buttonText = String_val(Some_val(vButtonText));
-  } else {
-    buttonText = NULL;
-  }
+    if (vButtonText != Val_none) {
+        buttonText = String_val(Some_val(vButtonText));
+    } else {
+        buttonText = NULL;
+    }
 
-  char **fileList = NULL;
+    char **fileList = NULL;
 
 #ifdef USE_WIN32
-  fileList = revery_open_files_win32(startDirectory, canChooseFiles,
-                                     canChooseDirectories, title);
-  (void)fileTypesSize;
-  (void)allowMultiple;
-  (void)showHidden;
-  (void)title;
-  (void)buttonText;
+    fileList = revery_open_files_win32(startDirectory, canChooseFiles,
+                                       canChooseDirectories, title);
+    (void)fileTypesSize;
+    (void)allowMultiple;
+    (void)showHidden;
+    (void)title;
+    (void)buttonText;
 #elif USE_COCOA
-  fileList = revery_open_files_cocoa(
-      startDirectory, fileTypes, fileTypesSize, allowMultiple, canChooseFiles,
-      canChooseDirectories, showHidden, buttonText, title);
+    fileList = revery_open_files_cocoa(
+                   startDirectory, fileTypes, fileTypesSize, allowMultiple, canChooseFiles,
+                   canChooseDirectories, showHidden, buttonText, title);
 #elif USE_GTK
-  fileList = revery_open_files_gtk(
-      startDirectory, fileTypes, fileTypesSize, allowMultiple, canChooseFiles,
-      canChooseDirectories, showHidden, buttonText, title);
+    fileList = revery_open_files_gtk(
+                   startDirectory, fileTypes, fileTypesSize, allowMultiple, canChooseFiles,
+                   canChooseDirectories, showHidden, buttonText, title);
 #else
-  (void)showHidden;
-  (void)canChooseDirectories;
-  (void)canChooseFiles;
-  (void)allowMultiple;
-  (void)title;
-  (void)buttonText;
-  (void)startDirectory;
-  (void)fileList;
+    (void)showHidden;
+    (void)canChooseDirectories;
+    (void)canChooseFiles;
+    (void)allowMultiple;
+    (void)title;
+    (void)buttonText;
+    (void)startDirectory;
+    (void)fileList;
 #endif
 
-  if (fileList) {
-    CAMLlocal1(camlArr);
-    int len = -1;
-    while (fileList[++len] != NULL) {
+    if (fileList) {
+        CAMLlocal1(camlArr);
+        int len = -1;
+        while (fileList[++len] != NULL) {
+        }
+        camlArr = caml_alloc(len, 0);
+
+        for (int i = 0; i < len; i++) {
+            Store_field(camlArr, i, caml_copy_string(fileList[i]));
+        }
+
+        free(fileList);
+
+        CAMLreturn(Val_some(camlArr));
+    } else {
+        CAMLreturn(Val_none);
     }
-    camlArr = caml_alloc(len, 0);
-
-    for (int i = 0; i < len; i++) {
-      Store_field(camlArr, i, caml_copy_string(fileList[i]));
-    }
-
-    free(fileList);
-
-    CAMLreturn(Val_some(camlArr));
-  } else {
-    CAMLreturn(Val_none);
-  }
 }
 
 CAMLprim value revery_alertOpenFiles_bytecode(value *argv, int argn) {
-  (void)argn;
-  return revery_alertOpenFiles_native(argv[0], argv[1], argv[2], argv[3],
-                                      argv[4], argv[5], argv[6], argv[7],
-                                      argv[8]);
+    (void)argn;
+    return revery_alertOpenFiles_native(argv[0], argv[1], argv[2], argv[3],
+                                        argv[4], argv[5], argv[6], argv[7],
+                                        argv[8]);
 }
