56973912fa0a24720ad92143cf6204d6bca8bd2f
Bryan Phelps <bryphe@outrunlabs.com>
2021-05-11 12:41:42 -0700

    Use Unicode encoding for dir / file conversion

--- src/Native/dialog_cocoa.c
+++ src/Native/dialog_cocoa.c
@@ -86,6 +86,7 @@ const char **revery_open_files_cocoa(
             We NULL terminate it so we can get the size in the main function
         */
         int size = [urls count];
+        int actualSize = 0;
         const char **ret = malloc((size + 1) * sizeof(char *));
         // Copy the NSArray to the C-array
         for (int i = 0; i < size; i++) {
@@ -96,10 +97,13 @@ const char **revery_open_files_cocoa(
             // the [NSString]:
             // https://developer.apple.com/documentation/foundation/nsstring/1408489-cstringusingencoding?language=objc
             // So we need to make a copy here...
-            ret[i] = strdup(sz);
+            if (sz != NULL) {
+                ret[actualSize] = strdup(sz);
+                actualSize++;
+            }
         }
         [urls release];
-        ret[size] = NULL;
+        ret[actualSize] = NULL;
         [keyWindow makeKeyWindow];
         return ret;
     } else {
